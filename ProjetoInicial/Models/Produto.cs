2c9cb39366d398b59ed08c43365bbb3038b251ad
Getulio <augustoge71@gmail.com>
2023-11-16 23:24:38 -0300

    Concluída etapa de construção/validação de formulários.

--- ProjetoInicial/Models/Produto.cs
+++ ProjetoInicial/Models/Produto.cs
@@ -1,9 +1,16 @@
-﻿namespace ProjetoInicial.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoInicial.Models
 {
     public class Produto
     {
-        public string Nome { get; set; }
+        [Required(ErrorMessage ="Campo obrigatório!")] public string Nome { get; set; }
+
+        [Range(0, 9999999, ErrorMessage = "Informe a quantidade adequada!")]
         public int Quantidade { get; set; }
+
+        [Range(0.1, 9999999, ErrorMessage = "Informe o preço adequado!")]
         public double Preco {  get; set; }
 
         public double ValorEstoqueTotal()
